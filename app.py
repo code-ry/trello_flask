98f5880a540250d2469d1a0b65412c26be10dd30
code-ry <rybussey@gmail.com>
2022-10-13 10:02:15 +1000

    Added cards

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from datetime import date
 
 app = Flask(__name__)
 
@@ -7,8 +8,66 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://trello_dev:passwo
 
 db = SQLAlchemy(app)
 
-print(db.__dict__)
+class Card(db.Model):
+    __tablename__ = 'cards'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    description = db.Column(db.Text)
+    date = db.Column(db.Date)
+    status = db.Column(db.String)
+    priority = db.Column(db.String)
+
+
+# Define a custom CLI (terminal) command
+@app.cli.command('create')
+def create_db():
+    db.create_all()
+    print("Tables created")
+
+@app.cli.command('drop')
+def drop_db():
+    db.drop_all()
+    print("Tables dropped")
+
+@app.cli.command('seed')
+def seed_db():
+    cards = [
+        Card(
+            title = 'Start the project',
+            description = 'Stage 1 - Create the database',
+            status = 'To Do',
+            priority = 'High',
+            date = date.today()
+        ),
+        Card(
+            title = "SQLAlchemy",
+            description = "Stage 2 - Integrate ORM",
+            status = "Ongoing",
+            priority = "High",
+            date = date.today()
+        ),
+        Card(
+            title = "ORM Queries",
+            description = "Stage 3 - Implement several queries",
+            status = "Ongoing",
+            priority = "Medium",
+            date = date.today()
+        ),
+        Card(
+            title = "Marshmallow",
+            description = "Stage 4 - Implement Marshmallow to jsonify models",
+            status = "Ongoing",
+            priority = "Medium",
+            date = date.today()
+        )
+    ]
+
+    db.session.add_all(cards)
+    db.session.commit()
+    print('Tables seeded')
+
 
 @app.route('/')
 def index():
-    return "Whats up Doc, this is a change"
+    return "Whats up Doc!?"
